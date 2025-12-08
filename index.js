26dfdcfae87138c59cf9d18dffdfce80a95eaab1
Mahialam Rahat <mahialam408@gmail.com>
2025-12-08 20:56:09 +0600

    payment for services

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET}`);
 
 
 // MiddleWare
@@ -44,10 +45,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-      
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        
 
       // Crud Operations
       const db = client.db('style_decor_db');
@@ -144,6 +145,54 @@ async function run() {
       }
 
     })
+
+
+    // Stripe payment related apis
+    app.post('/create-checkout-session', async (req, res) => {
+      const { email, serviceId, booking_id, booking_quantity } = req.body;
+     
+      const query = { _id: new ObjectId(serviceId) }
+      const booked_services_info = await servicesCollection.findOne(query);
+      const booking_cost = booked_services_info.cost;
+
+
+      // Convert BDT → USD cents
+      const usdCents = Math.round(booking_cost / 127.1409 * 100);
+
+      const finalAmount = Math.max(usdCents, 50);
+    
+
+     console.log('in booked', booking_cost, usdCents, finalAmount)
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: "Test Hardcoded Service"
+              },
+              unit_amount: finalAmount
+            },
+            quantity: 1,
+          },
+        ],
+
+        metadata: {
+          booking_id: booking_id,
+          serviceId: serviceId,
+          serviceName: booked_services_info?.service_name
+        },
+        customer_email: email,
+        mode: 'payment',
+        success_url: `${process.env.SITE_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_DOMAIN}/payment-cancel`,
+      })
+
+      res.send({url: session.url})
+      
+    });
       
 
 
